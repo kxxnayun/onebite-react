a164ffd2d2d5d47a524488e33ffee39117f003ce
kimnayun <nayun2179@gmail.com>
2025-10-01 17:31:25 +0900

    section2 js 심화

---
+++ section02/chapter07.js
@@ -0,0 +1,13 @@
+// 6가지의 요소 조작 메서드
+
+// 1. push
+// 배열의 맨 뒤에 새로운 요소를 추가하는 메서드
+let arr1 = [1, 2, 3];
+const newLength = arr1.push(4, 5, 6, 7);
+
+console.log(arr1);
+console.log(newLength);
+
+// 2. pop
+// 배열의 맨 뒤에 있는 요소를 제거하고, 반환
+let arr2 = [1, 2, 3];
